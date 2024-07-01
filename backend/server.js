e15113277bfb21a225942859396318653fa52dcb
sumaiyashafeek123 <sumaiyashafeek123123@gmail.com>
2024-07-01 17:40:58 +0530

    adminhomepage

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // Define CORS options
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your frontend URL
-  methods: ['GET', 'POST'], // Specify allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  origin: "http://localhost:5173", // Replace with your frontend URL
+  methods: ["GET", "POST"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
 };
 
 // Enable CORS with specific options
@@ -24,16 +24,22 @@ app.use((req, res, next) => {
 });
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 4000, () => {
-      console.log("DB Connected Successfully. Listening on port " + (process.env.PORT || 4000));
+      console.log(
+        "DB Connected Successfully. Listening on port " +
+          (process.env.PORT || 4000)
+      );
     });
   })
-  .catch((error) => console.log('DB connection error:', error));
+  .catch((error) => console.log("DB connection error:", error));
 
 // Routes
-const locomotivePilotRoute = require('./routes/locomotivePilotRoute');
+const locomotivePilotRoute = require("./routes/locomotivePilotRoute");
+const AdministrativeOfficerRoute = require("./routes/AdministrativeOfficerRoute");
 
 // Connect routes
+app.use("/api/AdministrativeOfficer", AdministrativeOfficerRoute);
 app.use("/api/locomotivePilot", locomotivePilotRoute);
--- frontend/src/pages/react/AdminHomePage .jsx
+++ frontend/src/pages/react/AdminHomePage .jsx
@@ -1,87 +1,132 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "./../style/AdminHomePage .css";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import hazardimage from '../assets/hazard.jpeg';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import hazardlocationimage from "../assets/HazardLocation.jpg";
+import approvehazardimage from "../assets/ApproveHazard.jpg";
+import locomotivepilotimage from "../assets/LocomotivePilot.jpg";
+import updatehazardimage from "../assets/UpdateHazard.jpg";
 
 function AdminHomePage() {
   return (
     <>
-   
-   <div className="AdminHomePage-left-container-flex ">
-  <div className="row">
+      <div className="AdminHomePage-left-container-flex ">
+        <div className="row">
+          <div className="col-12 AdminHomePage-heading-box vh-20">
+            <h1 className="AdminHomePage-heading-title ">Admin Home Page</h1>
+          </div>
+        </div>
+      </div>
 
-    <div className="col-12 AdminHomePage-heading-box vh-20">
-      <h1 className="AdminHomePage-heading-title ">Admin Home Page</h1>
-    </div>
-  </div>
-</div>
-
-<div className="AdminHomePage-right-container-flex ">
-  <div className="row">
-    <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-1">
-        <div className="AdminHomePage-inserted-card-box-1"> 
-          <Card style={{ width: '100%',height:'100%',borderWidth:5,}} className="AdminHomePage-inserted-card">
-            <Card.Img variant="top" src={hazardimage} className="AdminHomePage-inserted-card-box-image"/>
-              <Card.Body>
-              <Card.Title className="AdminHomePage-inserted-card-box-title">Approve Hazard</Card.Title>
-              <Card.Text className="AdminHomePage-inserted-card-box-text">
-               Some quick example text to build on the card title and make up the
-               bulk of the card's content.
-            </Card.Text>
-              <Button className="AdminHomePage-inserted-card-box-button">Go somewhere</Button>
-           </Card.Body>
-          </Card>
+      <div className="AdminHomePage-right-container-flex ">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-1">
+            <div className="AdminHomePage-inserted-card-box-1">
+              <Card
+                style={{ width: "100%", height: "100%", borderWidth: 5 }}
+                className="AdminHomePage-inserted-card"
+              >
+                <Card.Img
+                  variant="top"
+                  src={approvehazardimage}
+                  className="AdminHomePage-inserted-card-box-image"
+                />
+                <Card.Body>
+                  <Card.Title className="AdminHomePage-inserted-card-box-title">
+                    Approve Hazard
+                  </Card.Title>
+                  <Card.Text className="AdminHomePage-inserted-card-box-text">
+                    This will display the newly reported hazards and details of
+                    the reporting locomotive pilot.
+                  </Card.Text>
+                  <Button className="AdminHomePage-inserted-card-box-button">
+                    Go somewhere
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-2">
+            <div className="AdminHomePage-inserted-card-box-2">
+              <Card
+                style={{ width: "100%", height: "100%", borderWidth: 5 }}
+                className="AdminHomePage-inserted-card"
+              >
+                <Card.Img
+                  variant="top"
+                  src={hazardlocationimage}
+                  className="AdminHomePage-inserted-card-box-image"
+                />
+                <Card.Body>
+                  <Card.Title className="AdminHomePage-inserted-card-box-title">
+                    Hazard location
+                  </Card.Title>
+                  <Card.Text className="AdminHomePage-inserted-card-box-text">
+                    The administrative officer can update hazards here, which
+                    locomotive pilot fails to report.
+                  </Card.Text>
+                  <Button className="AdminHomePage-inserted-card-box-button">
+                    Go somewhere
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-3">
+            <div className="AdminHomePage-inserted-card-box-3">
+              <Card
+                style={{ width: "100%", height: "100%", borderWidth: 5 }}
+                className="AdminHomePage-inserted-card"
+              >
+                <Card.Img
+                  variant="top"
+                  src={locomotivepilotimage}
+                  className="AdminHomePage-inserted-card-box-image"
+                />
+                <Card.Body>
+                  <Card.Title className="AdminHomePage-inserted-card-box-title">
+                    Locomotive pilot Details
+                  </Card.Title>
+                  <Card.Text className="AdminHomePage-inserted-card-box-text">
+                    Administrative officers can use this interface to update
+                    locomotive pilot details.
+                  </Card.Text>
+                  <Button className="AdminHomePage-inserted-card-box-button">
+                    Go somewhere
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-4">
+            <div className="AdminHomePage-inserted-card-box-4">
+              <Card
+                style={{ width: "100%", height: "100%", borderWidth: 5 }}
+                className="AdminHomePage-inserted-card"
+              >
+                <Card.Img
+                  variant="top"
+                  src={updatehazardimage}
+                  className="AdminHomePage-inserted-card-box-image"
+                />
+                <Card.Body>
+                  <Card.Title className="AdminHomePage-inserted-card-box-title">
+                    Admin approve hazard location
+                  </Card.Title>
+                  <Card.Text className="AdminHomePage-inserted-card-box-text">
+                    Admin can approve and update hazards which is reported by
+                    locomotive pilots.
+                  </Card.Text>
+                  <Button className="AdminHomePage-inserted-card-box-button">
+                    Go somewhere
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
         </div>
-    </div>
-    <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-2">
-        <div className="AdminHomePage-inserted-card-box-2"> 
-        <Card style={{ width: '100%',height:'100%',borderWidth:5,}} className="AdminHomePage-inserted-card">
-            <Card.Img variant="top" src={hazardimage} className="AdminHomePage-inserted-card-box-image"/>
-              <Card.Body>
-              <Card.Title className="AdminHomePage-inserted-card-box-title">Approve Hazard</Card.Title>
-              <Card.Text className="AdminHomePage-inserted-card-box-text">
-               Some quick example text to build on the card title and make up the
-               bulk of the card's content.
-            </Card.Text>
-              <Button className="AdminHomePage-inserted-card-box-button">Go somewhere</Button>
-           </Card.Body>
-          </Card>
-        </div>
-    </div>
-    <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-3">
-        <div className="AdminHomePage-inserted-card-box-3"> 
-        <Card style={{ width: '100%',height:'100%',borderWidth:5,}} className="AdminHomePage-inserted-card">
-            <Card.Img variant="top" src={hazardimage} className="AdminHomePage-inserted-card-box-image"/>
-              <Card.Body>
-              <Card.Title className="AdminHomePage-inserted-card-box-title">Approve Hazard</Card.Title>
-              <Card.Text className="AdminHomePage-inserted-card-box-text">
-               Some quick example text to build on the card title and make up the
-               bulk of the card's content.
-            </Card.Text>
-              <Button className="AdminHomePage-inserted-card-box-button">Go somewhere</Button>
-           </Card.Body>
-          </Card>
-        </div>
-    </div>
-    <div className="col-sm-12 col-md-6 col-lg-3 AdminHomePage-card-box-4">
-        <div className="AdminHomePage-inserted-card-box-4"> 
-        <Card style={{ width: '100%',height:'100%',borderWidth:5,}} className="AdminHomePage-inserted-card">
-            <Card.Img variant="top" src={hazardimage} className="AdminHomePage-inserted-card-box-image"/>
-              <Card.Body>
-              <Card.Title className="AdminHomePage-inserted-card-box-title">Approve Hazard</Card.Title>
-              <Card.Text className="AdminHomePage-inserted-card-box-text">
-               Some quick example text to build on the card title and make up the
-               bulk of the card's content.
-            </Card.Text>
-              <Button className="AdminHomePage-inserted-card-box-button">Go somewhere</Button>
-           </Card.Body>
-          </Card>
-        </div>
-    </div>
-  </div>
-</div>
+      </div>
     </>
   );
 }
